7a1ad910cfdb1230b92a6681f11e944c70a19bdf
TraciK <70920574+TraciK@users.noreply.github.com>
2025-08-27 02:45:11 +0000

    make suggested updates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,21 @@
 class User < ApplicationRecord
-  has_many :user_responses
-  has_many :study_sessions
+  has_secure_password
+  
+  has_many :user_responses, dependent: :destroy
+  has_many :study_sessions, dependent: :destroy
+  
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 6 }, if: :password_required?
+  
+  before_save :downcase_email
+  
+  private
+  
+  def downcase_email
+    self.email = email.downcase
+  end
+  
+  def password_required?
+    new_record? || password.present?
+  end
 end
