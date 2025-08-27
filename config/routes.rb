a93257f4d3bea006e805dd9e0098cee7e51f51ba
TraciK <70920574+TraciK@users.noreply.github.com>
2025-08-27 02:33:09 +0000

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
-  # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
-  # get("/your_first_screen", { :controller => "pages", :action => "first" })
+  root 'practice#index'
+  
+  get '/practice', to: 'practice#show'
+  post '/practice/check', to: 'practice#check_answer'
+  
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  
+  get '/register', to: 'users#new'
+  post '/register', to: 'users#create'
+  
+  get '/stats', to: 'users#stats'
+  get '/letters', to: 'letters#index'
 end
